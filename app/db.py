8936827cdfb229d15072822003848091ba20ef3d
Vadym Hutei <vadymhutei@outlook.com>
2019-01-17 16:47:09 +0200

    [DB]

--- app/db.py
+++ app/db.py
@@ -8,32 +8,8 @@ class DB():
         self.db_params['cursorclass'] = pymysql.cursors.DictCursor
         self.pefix = config.db_prefix
 
-    def _create_connection(self):
-        connection = pymysql.connect(**self.db_params)
-        return connection
+    def getConnection(self):
+        return pymysql.connect(**self.db_params)
 
     def table(self, table_name):
         return self.pefix + table_name
-
-    def exec(self, query, *args):
-        connection = self._create_connection()
-        cursor = connection.cursor()
-        cursor.execute(query, args)
-        connection.commit()
-        connection.close()
-
-    def getOne(self, query, *args):
-        connection = self._create_connection()
-        cursor = connection.cursor()
-        cursor.execute(query, args)
-        result = cursor.fetchone()
-        connection.close()
-        return result
-
-    def getAll(self, query, *args):
-        connection = self._create_connection()
-        cursor = connection.cursor()
-        cursor.execute(query, args)
-        result = cursor.fetchall()
-        connection.close()
-        return result
