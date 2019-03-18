ec2aaeee7339778a8edd343bc9c2223fe7ed315f
Vadym Hutei <hutei@live.com>
2019-03-18 15:15:38 +0200

    [CONFIG] languages

--- app/config.py
+++ app/config.py
@@ -5,17 +5,23 @@ except:
 from db import DB
 
 # LANGUAGES
+LANGUAGES = {}
+DEFAULT_LANGUAGE = False
 db = DB()
-query = 'SELECT `code`, `is_default` FROM `{table}`'.format(table=db.table('languages'))
+query = """
+    SELECT
+        `code`,
+        `name`,
+        `is_default`
+    FROM `{table}`
+""".format(table=db.table('languages'))
 connection = db.getConnection()
 cursor = connection.cursor()
 cursor.execute(query)
 connection.close()
-LANGUAGES = []
-DEFAULT_LANGUAGE = False
 languages_data = cursor.fetchall()
 if languages_data:
     for row in languages_data:
-        LANGUAGES.append(row['code'])
+        LANGUAGES[row['code']] = row
         if row['is_default'] == 'Y' or not DEFAULT_LANGUAGE:
             DEFAULT_LANGUAGE = row['code']
