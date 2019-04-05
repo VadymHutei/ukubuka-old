f79404efbcd055e3951b154f8fa65a257d8e9027
Vadym Hutei <hutei@live.com>
2019-04-05 14:29:43 +0300

    [VALIDATION]

--- app/modules/validation.py
+++ app/modules/validation.py
@@ -2,22 +2,22 @@ import re
 
 def phoneNumber(phone_number):
     if isinstance(phone_number, str):
-        return phone_number == '' or re.fullmatch(r'[0-9+() -]{9,64}', phone_number)
+        return re.fullmatch(r'[0-9+() -]{9,64}', phone_number)
     return False
 
 def email(email):
     if isinstance(email, str):
-        return email == '' or re.fullmatch(r'[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+', email)
+        return re.fullmatch(r'[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+', email)
     return False
 
 def settingsProperty(prop):
     if isinstance(prop, str):
-        return re.fullmatch(r'[\w. -]{,64}', prop)
+        return re.fullmatch(r'[\w. -]{1,64}', prop)
     return False
 
 def password(prop):
     if isinstance(prop, str):
-        return prop == '' or re.fullmatch(r'[\w\W]{3,64}', prop)
+        return re.fullmatch(r'[\w\W]{3,64}', prop)
     return False
 
 def sessionId(session_id):
@@ -27,7 +27,12 @@ def sessionId(session_id):
 
 def price(price):
     if isinstance(price, str):
-        return re.fullmatch(r'[0-9 .,]{,16}', price)
+        return re.fullmatch(r'[0-9 .,]{1,16}', price)
+    return False
+
+def order(order):
+    if isinstance(order, str):
+        return re.fullmatch(r'[0-9]{1,4}', order)
     return False
 
 #     ##     ## ######## ##    ## ##     ##
@@ -45,12 +50,12 @@ def menuItemID(item_id):
 
 def menuName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'\w{,32}', name)
+        return re.fullmatch(r'\w{1,32}', name)
     return False
 
 def menuItemName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{,64}', name)
+        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{1,64}', name)
     return False
 
 def menuItemLink(link):
@@ -78,12 +83,12 @@ def usersGroupID(users_group_id):
 
 def userName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'\w{,64}', name)
+        return re.fullmatch(r'\w{1,64}', name)
     return False
 
 def usersGroupName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'\w{,64}', name)
+        return re.fullmatch(r'\w{1,64}', name)
     return False
 
 #      ######     ###    ######## ########  ######    #######  ########  ##    ##
@@ -101,7 +106,7 @@ def categoryID(category_id):
 
 def categoryName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'\w{,64}', name)
+        return re.fullmatch(r'\w{1,64}', name)
     return False
 
 #     ########  ########   #######  ########  ##     ##  ######  ########  ######
@@ -114,17 +119,17 @@ def categoryName(name):
 
 def productModel(model):
     if isinstance(model, str):
-        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{,64}', model)
+        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{1,64}', model)
     return False
 
 def productName(name):
     if isinstance(name, str):
-        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{,128}', name)
+        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{1,128}', name)
     return False
 
 def productDescrioption(description):
     if isinstance(description, str):
-        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{,256}', description)
+        return re.fullmatch(r'[0-9A-Za-zА-Яа-яЯєЄіІїЇёЁҐґ _-]{1,256}', description)
     return False
 
 #      ######  ##     ##    ###    ########     ###     ######  ######## ######## ########  ####  ######  ######## ####  ######
