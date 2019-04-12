f6543f5927a67f82433388e1888da6fcb115c5ac
Vadym Hutei <hutei@live.com>
2019-04-12 15:55:30 +0300

    [CONFIG] fixed languages, added currencies

--- app/config.py
+++ app/config.py
@@ -5,15 +5,20 @@ except:
 from db import DB
 
 # LANGUAGES
-LANGUAGES = {}
+LANGUAGES = []
+LANGUAGES_DATA = []
 DEFAULT_LANGUAGE = False
 db = DB()
 query = """
     SELECT
         `code`,
         `name`,
-        `is_default`
+        `is_default`,
+        `order`,
+        `added`,
+        `is_active`
     FROM `{table}`
+    ORDER BY `order` DESC
 """.format(table=db.table('languages'))
 connection = db.getConnection()
 cursor = connection.cursor()
@@ -22,6 +27,32 @@ connection.close()
 languages_data = cursor.fetchall()
 if languages_data:
     for row in languages_data:
-        LANGUAGES[row['code']] = row
+        LANGUAGES.append(row['code'])
+        LANGUAGES_DATA.append(row)
         if row['is_default'] == 'Y' or not DEFAULT_LANGUAGE:
             DEFAULT_LANGUAGE = row['code']
+
+# CURRENCIES
+CURRENCIES = []
+CURRENCIES_DATA = []
+db = DB()
+query = """
+    SELECT
+        `code`,
+        `name`,
+        `symbol`,
+        `order`,
+        `added`,
+        `is_active`
+    FROM `{table}`
+    ORDER BY `order` DESC
+""".format(table=db.table('currencies'))
+connection = db.getConnection()
+cursor = connection.cursor()
+cursor.execute(query)
+connection.close()
+currencies_data = cursor.fetchall()
+if currencies_data:
+    for row in currencies_data:
+        CURRENCIES.append(row['code'])
+        CURRENCIES_DATA.append(row)
