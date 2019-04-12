4ea211de6cb5cee9499d72c1ead9d14bda830cd8
Vadym Hutei <hutei@live.com>
2019-04-12 15:56:33 +0300

    [LIB] fixed parsePrice

--- app/ulib/common.py
+++ app/ulib/common.py
@@ -1,13 +1,19 @@
 import re
 
 def parsePrice(data):
-    if not isinstance(data, str): return 0
-    temp = data.replace(',', '.')
-    coma_position = temp.rfind('.')
-    foo = (temp,) if coma_position == -1 else (temp[:coma_position], temp[coma_position:])
-    bar = [''.join(re.findall(r'[0-9]', part)) for part in foo]
-    price = int(bar[0]) * 100 if bar[0] else 0
-    if len(bar) > 1: price += int(bar[1][:2])
+    if isinstance(data, str):
+        temp = data.replace(',', '.')
+        coma_position = temp.rfind('.')
+        foo = (temp,) if coma_position == -1 else (temp[:coma_position], temp[coma_position:])
+        bar = [''.join(re.findall(r'[0-9]', part)) for part in foo]
+        price = int(bar[0]) * 100 if bar[0] else 0
+        if len(bar) > 1: price += int(bar[1][:2])
+    elif isinstance(data, int):
+        price = price * 100
+    elif isinstance(data, float):
+        price = round(price * 100)
+    else:
+        price = None
     return price
 
 def formatPrice(data):
